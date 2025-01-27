7f67086e8ff0ca25855ec5d0910f200b65398316
AdieDev0 <devutella@gmail.com>
2025-01-27 23:27:48 +0800

    Adding styles

--- tastemnl/src/Pages/Navbar.jsx
+++ tastemnl/src/Pages/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <div>
+        {/* NAME */}
+        <h1></h1>
+        {/* Routes */}
+        <div>
+          <h1>HOME</h1>
+          <h1>ABOUT</h1>
+          <h1>SERVICE</h1>
+          <h1>MENU</h1>
+          <h1>PAGES</h1>
+          <h1>CONTACT</h1>
+          <button className="cursor-pointer">BOOK A TABLE</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
