dbad53e3c4424ec0c18380942db1a23070f6fe65
AdieDev0 <devutella@gmail.com>
2025-01-27 23:21:26 +0800

    Set up react router dom

--- tastemnl/src/App.jsx
+++ tastemnl/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Pages/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
   );
 };
 
