f40c14d1fb063f913c6ec0f39a9f62fce64acefd
AdieDev0 <devutella@gmail.com>
2025-01-28 01:36:36 +0800

    Enhance

--- tastemnl/src/Components/Navbar.jsx
+++ tastemnl/src/Components/Navbar.jsx
@@ -27,11 +27,11 @@ const Navbar = () => {
           </li>
           <Menu as="div" className="relative inline-block text-left">
             <div>
-              <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50">
-                Options
+              <Menu.Button className="inline-flex w-full justify-center items-center gap-x-1.5 text-white hover:text-yellow-600 duration-200">
+                PAGES
                 <FiChevronDown
                   aria-hidden="true"
-                  className="-mr-1 text-lg text-gray-400"
+                  className="-mr-1 text-lg text-white hover:text-yellow-600 duration-200"
                 />
               </Menu.Button>
             </div>
