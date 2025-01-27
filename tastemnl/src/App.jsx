c3026bb7def063ad740b560b03439fa68f27ebd2
AdieDev0 <devutella@gmail.com>
2025-01-28 01:13:28 +0800

    Add routes

--- tastemnl/src/App.jsx
+++ tastemnl/src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Pages/Navbar";
+import Navbar from "./Components/Navbar";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Service from "./Pages/Service";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
